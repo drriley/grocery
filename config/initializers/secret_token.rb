cd35d04c91e3c11d6b4ee95fb937b6bb8bd738c4
Darren <drriley@andrew.cmu.edu>
2013-02-25 02:09:46 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grocery::Application.config.secret_token = '2d89271210b337c80bc8ae9b73ea8facf85fc3aac35072729e9239867425bbeb2501458d5299309421812a53ab26293c03450a91a7659c6ceb23c826f97ed505'
