0967d90d8046533827343aeda3b369336be5c658
Julia Teitelbaum <julialt@gmail.com>
2013-03-26 18:14:21 -0400

    created shopping list items controller

--- app/models/shopping_list.rb
+++ app/models/shopping_list.rb
@@ -5,4 +5,8 @@ class ShoppingList < ActiveRecord::Base
   belongs_to :customer
   has_many :shopping_list_items
   has_many :items, :through => :shopping_list_items
+
+  # Validations
+  validates_presence_of :customer_id
+  
 end
--- app/models/shopping_list_item.rb
+++ app/models/shopping_list_item.rb
@@ -1,12 +1,23 @@
 class ShoppingListItem < ActiveRecord::Base
-  attr_accessible :item_id, :purchased_yet, :shopping_list_id, :visible_in_list
-
-  # Relationships
-  belongs_to :item
-  belongs_to :shopping_list, :dependent => :destroy
-
-  # Validations
-  validates_presence_of :item_id, :shopping_list_id
+	attr_accessible :item_id, :purchased_yet, :shopping_list_id, :visible_in_list
 
 
+	# Relationships
+
+	belongs_to :item
+	belongs_to :shopping_list, :dependent => :destroy
+
+
+	# Validations
+
+	validates_presence_of :item_id, :shopping_list_id
+
+
+	# Methods
+
+	# get the name of the item by looking at the associated item record
+	def name
+		self.item.name
+	end
+	
 end
