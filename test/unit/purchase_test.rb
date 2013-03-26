e300d8cf2542be92179ec1971ed35dbc0c110902
Matthew Hsieh <mehsieh@andrew.cmu.edu>
2013-03-26 18:17:41 -0400

    created purchase tests

--- test/unit/purchase_test.rb
+++ test/unit/purchase_test.rb
@@ -9,4 +9,28 @@ class PurchaseTest < ActiveSupport::TestCase
   should validate_presence_of(:date)
   
   should allow_value("01-01-2013").for(:date)
+  
+  context "creating 2 purchases on different dates" do 
+    setup do
+      @leonard = Factorygirl.create(:customer)
+      @purchase1 = Factorygirl.create(:purchase)
+      @purchase2 = Factorygirl.create(:purchase, :date => 1.day.ago.to_date)
+    end 
+    
+    teardown do
+      @leonard.destroy
+      @purchase1.destroy
+      @purchase2.destroy
+      @purchase3.destroy
+    end
+    
+    should "have dates" do
+      assert_equal Date.today, @purchase1.date
+      assert_equal 1.day.ago.to_date, @purchase2.date
+      assert_not_equal 2.day.ago.to_date, @purchase2.date
+      assert_not_equal 2, @purchase2.date
+    end
+    
+  end
+  
 end
