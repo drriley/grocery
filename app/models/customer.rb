5805c06ccfc94568408030a940dd70cf01d512e5
Leonard L. Sokol <lsokol@andrew.cmu.edu>
2013-03-30 18:41:45 -0400

    added rudimentary vlaidations and improved email validation. need to still figure out how validates_presence_of and allow_blank overlap/interfere

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -11,11 +11,13 @@ class Customer < ActiveRecord::Base
   scope :active, where('active = ?', true)
   scope :inactive, where('active = ?', false)
  
+ #### TODO: need to figure out the parameter diff between allow_blank and validates_presence_of
+ 
   # validations 
-  validates_presence_of :first_name, :last_name
-  validates_format_of :phone, :with => /^(\d{10}|\(?\d{3}\)?[-. ]\d{3}[-.]\d{4})$/, :message => "should be 10 digits (area code needed) and delimited with dashes only"
-  validates_format_of :email, :with => /^[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info))$/i, :message => "is not a valid format"
-  validates_format_of :zip, :with => /^\d{5}$/, :message => "should be five digits long", :allow_blank => true
+  validates_presence_of :first_name, :last_name, :street, :phone, :email, :zip
+  validates_format_of :phone, :with => /^(\d{10}|\(?\d{3}\)?[-. ]\d{3}[-.]\d{4})$/, :message => "should be 10 digits (area code needed) and delimited with dashes only", :allow_blank => false
+  validates_format_of :email, :with => /^[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info))$/i, :message => "is not a valid format", :allow_blank => false
+  validates_format_of :zip, :with => /^\d{5}$/, :message => "should be five digits long", :allow_blank => false
   
 end
 
