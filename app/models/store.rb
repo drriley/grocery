b68cc11630e99a72b5fc6351bbd0de25b1094806
Leonard L. Sokol <lsokol@andrew.cmu.edu>
2013-03-30 18:29:50 -0400

    added simple validations to store model

--- app/models/store.rb
+++ app/models/store.rb
@@ -5,4 +5,8 @@ class Store < ActiveRecord::Base
   belongs_to :company
   has_many :item_stores
   
+  # if zip included, it must be 5 digits only
+  validates_format_of :zip, :with => /^\d{5}$/, :message => "should be five digits long", :allow_blank => true
+  # phone can have dashes, spaces, dots and parens, but must be 10 digits
+  validates_format_of :phone, :with => /^(\d{10}|\(?\d{3}\)?[-. ]\d{3}[-.]\d{4})$/, :message => "should be 10 digits (area code needed) and delimited with dashes only"
 end
