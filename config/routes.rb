b6a8547bf85c22ebffac179c5ae484a95c73aecf
Julia Teitelbaum <julialt@gmail.com>
2013-04-11 02:16:47 -0400

    fixed merge conflicts in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -2,32 +2,13 @@ Grocery::Application.routes.draw do
 
   devise_for :users
 
-<<<<<<< HEAD
-	# added here lls
-=======
-  # added here lls
->>>>>>> devise
-  #	root :to => "home#index"	
-
   # custom routing for competitive intelligence data
   match 'item_purchases/company_intel' => 'item_purchases#company_intel', :defaults=>{:format =>'json'}
   
   resources :item_purchases
-<<<<<<< HEAD
   resources :shopping_list_items
 
   # inventory is homepage
   root :to => 'item_purchases#index'
-
-
-
-
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-=======
-  
-  root :to => 'item_purchases#index'
->>>>>>> devise
-
 
 end
