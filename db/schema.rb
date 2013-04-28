cfcb40d0f723055fdf0f3938096f8c04eca24cf9
Julia Teitelbaum <julialt@gmail.com>
2013-04-28 13:45:48 -0400

    added padding for homepage in stylesheets and made names better in the items.csv file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130408014106) do
+ActiveRecord::Schema.define(:version => 20130417005416) do
 
   create_table "companies", :force => true do |t|
     t.string   "name"
